c5551ac89e39fa5f0c81a3e06f335562ee7f8dd2
MJR2010gbc <113392635+MJR2010gbc@users.noreply.github.com>
2023-03-22 18:43:19 -0400

    Update CSLDA.c

--- Computer Graphics/Cohen Sutherland Line Drawing Algorithm/CSLDA.c
+++ Computer Graphics/Cohen Sutherland Line Drawing Algorithm/CSLDA.c
@@ -1,3 +1,7 @@
+//the Cohen–Sutherland algorithm is an algorithm used for line clipping. 
+//The algorithm divides a two-dimensional space into 9 regions and then efficiently determines the lines and portions of lines that are visible in the central region of interest (the viewport).
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
